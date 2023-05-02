3b6b8bb1f2dd09f4461598687f52e24ae1e0ae1a
Okko Ahonen <okko.a.ahonen@gmail.com>
2023-05-03 00:25:39 +0300

    ampuminen and rotaatio sun muut

--- ParsakaaliPeli/Assets/Scripts/Shoot.cs
+++ ParsakaaliPeli/Assets/Scripts/Shoot.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     public Transform shootpoint;
+    public float rotationSpeed;
     public GameObject bulletPreFab;
     
     void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        {
-            Instantiate(bulletPreFab, shootpoint.position, transform.rotation);
-        }
+        Debug.Log("Starttaa");
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Ampuu");
+            Instantiate(bulletPreFab, shootpoint.position, transform.rotation);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Rotate to the right
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            // Rotate to the left
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
