ce143f3e7726416d5eceb03d51aa15b506a87641
alekaixd <aleksi.pirttinen@gmail.com>
2023-05-03 13:56:06 +0300

    added carrot movement
    sprites get broken for whatever reason

--- ParsakaaliPeli/Assets/Scripts/CarrotMove.cs
+++ ParsakaaliPeli/Assets/Scripts/CarrotMove.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class CarrotMove : MonoBehaviour
 {
     public float speed = 8.0f;
-    public Rigidbody2D rb2d;
-    public GameObject player;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         gameObject.transform.LookAt(player.transform, Vector3.up);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // gameObject.transform.Translate(gameObject.transform.position - player.transform.position * speed * Time.deltaTime);
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed);
     }
 }
